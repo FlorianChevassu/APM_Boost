07f5acb49c08d136099fc9a229c938a1695f0752
CHEVASSU Florian <chevassu@TANB49.geomath.fr>
2015-10-13 21:48:21 +0200

    correct small bug in serach_compat call

--- search_compatible_version.cmake
+++ search_compatible_version.cmake
@@ -2,22 +2,22 @@ set(Boost_COMPATIBLE_VERSION_FOUND FALSE)
 
 IPM_get_compatible_package_version_root_parse_arguments(l_IPM_get_compatible_package_version_root ${ARGN})
 
-IPM_get_subdirectories(${Boost_PACKAGE_ROOT} l_IPM_version_dirs)
+IPM_get_subdirectories(${Boost_PACKAGE_ROOT} l_Boost_VERSION_dirs)
 
 #try to find a matching version
-foreach(l_IPM_version_dir ${l_IPM_version_dirs})
+foreach(l_Boost_VERSION_dir ${l_Boost_VERSION_dirs})
 	# first check that the project has been installed. If so, check version compatibility.
 	#TODO add path to test file
-	if(EXISTS ${Boost_PACKAGE_ROOT}/${l_IPM_version_dir}/install/)
-		if(${l_IPM_version_dir} VERSION_EQUAL ${IPM_VERSION})
+	if(EXISTS ${Boost_PACKAGE_ROOT}/${l_Boost_VERSION_dir}/install/)
+		if(${l_Boost_VERSION_dir} VERSION_EQUAL ${Boost_VERSION})
 			set(Boost_COMPATIBLE_VERSION_FOUND TRUE)
-			set(Boost_VERSION_ROOT ${Boost_PACKAGE_ROOT}/${l_IPM_version_dir})
+			set(Boost_VERSION_ROOT ${Boost_PACKAGE_ROOT}/${l_Boost_VERSION_dir})
 			break()
 		else()
 			#we assume that greater versions are backward compatible
-			if(${l_IPM_version_dir} VERSION_GREATER ${IPM_VERSION} AND NOT ${l_IPM_get_compatible_package_version_root_EXACT})
+			if(${l_Boost_VERSION_dir} VERSION_GREATER ${Boost_VERSION} AND NOT ${l_IPM_get_compatible_package_version_root_EXACT})
 				set(Boost_COMPATIBLE_VERSION_FOUND TRUE)
-				set(Boost_VERSION_ROOT ${Boost_PACKAGE_ROOT}/${l_IPM_version_dir})
+				set(Boost_VERSION_ROOT ${Boost_PACKAGE_ROOT}/${l_Boost_VERSION_dir})
 				break()
 			endif()
 		endif()
